3046ee10a02f96f6b3071ca3040362b56a155480
John Boyes <john@agilepathway.co.uk>
2020-12-24 05:40:06 +0000

    Fix golang linting errors

--- jira.go
+++ jira.go
@@ -14,29 +14,28 @@ import (
 )
 
 const (
-	localhost     = "localhost"
 	gaugeSpecsDir = "GAUGE_SPEC_DIRS"
-	gaugeApiPort  = "GAUGE_API_PORT"
 	fileSeparator = "||"
 )
 
-var projectRoot = util.GetProjectRoot()
-
-const tenGB = 1024 * 1024 * 1024 * 10
+var projectRoot = util.GetProjectRoot() //nolint:gochecknoglobals
 
 type handler struct {
 	server *grpc.Server
 }
 
 func (h *handler) GenerateDocs(c context.Context, m *gauge_messages.SpecDetails) (*gauge_messages.Empty, error) {
-	var files []string
+	var files []string //nolint:prealloc
 	for _, arg := range strings.Split(os.Getenv(gaugeSpecsDir), fileSeparator) {
 		files = append(files, util.GetFiles(arg)...)
 	}
+
 	for _, file := range files {
 		export.Spec(file)
 	}
-	fmt.Println("Succesfully exported specs to Jira")
+
+	fmt.Println("Successfully exported specs to Jira")
+
 	return &gauge_messages.Empty{}, nil
 }
 
@@ -50,18 +49,24 @@ func (h *handler) stopServer() {
 }
 
 func main() {
-	os.Chdir(projectRoot)
+	err := os.Chdir(projectRoot)
+	if err != nil {
+		util.Fatal("failed to start server.", err)
+	}
+
 	address, err := net.ResolveTCPAddr("tcp", "127.0.0.1:0")
 	if err != nil {
 		util.Fatal("failed to start server.", err)
 	}
+
 	l, err := net.ListenTCP("tcp", address)
 	if err != nil {
 		util.Fatal("failed to start server.", err)
 	}
-	server := grpc.NewServer(grpc.MaxRecvMsgSize(1024 * 1024 * 10))
+
+	server := grpc.NewServer(grpc.MaxRecvMsgSize(1024 * 1024 * 10)) //nolint:gomnd
 	h := &handler{server: server}
 	gauge_messages.RegisterDocumenterServer(server, h)
-	fmt.Println(fmt.Sprintf("Listening on port:%d", l.Addr().(*net.TCPAddr).Port))
-	server.Serve(l)
+	fmt.Printf("Listening on port:%d /n", l.Addr().(*net.TCPAddr).Port)
+	server.Serve(l) //nolint:errcheck,gosec
 }
